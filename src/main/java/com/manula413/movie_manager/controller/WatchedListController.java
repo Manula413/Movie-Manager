8cb9a2c9242daac6035db45cc51a343798cc27c3
Manula Pasan <89297752+Manula413@users.noreply.github.com>
2024-12-03 19:20:21 +0530

    WIP: Removed setupTableColumns and getWatchedListFromDB methods.

--- src/main/java/com/manula413/movie_manager/controller/WatchedListController.java
+++ src/main/java/com/manula413/movie_manager/controller/WatchedListController.java
@@ -38,79 +38,15 @@ public class WatchedListController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        setupTableColumns();
+     //   setupTableColumns();
         String username = Session.getInstance().getUsername();
         setUsernameLabel(username);
-        populateWatchedMoviesTable();
+     //   populateWatchedMoviesTable();
     }
 
     //
 
-    private void setupTableColumns() {
-        TableColumn<MovieDetails, String> titleCol = new TableColumn<>("Title");
-        titleCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getTitle()));
 
-        TableColumn<MovieDetails, String> yearCol = new TableColumn<>("Year");
-        yearCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getYear()));
-
-        TableColumn<MovieDetails, String> genreCol = new TableColumn<>("Genre");
-        genreCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getGenre()));
-
-        TableColumn<MovieDetails, String> typeCol = new TableColumn<>("Type");
-        typeCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getType()));
-
-        TableColumn<MovieDetails, String> seasonsCol = new TableColumn<>("Seasons");
-        seasonsCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getTotalSeasons()));
-
-        TableColumn<MovieDetails, String> imdbRatingCol = new TableColumn<>("IMDb Rating");
-        imdbRatingCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getImdbRating()));
-
-        TableColumn<MovieDetails, String> userCommentCol = new TableColumn<>("User Comment");
-        userCommentCol.setCellValueFactory(cellData -> javafx.beans.property.SimpleStringProperty(cellData.getValue().getPlot()));
-
-        watchedListTableView.getColumns().addAll(titleCol, yearCol, genreCol, typeCol, seasonsCol, imdbRatingCol, userCommentCol);
-    }
-
-    private ObservableList<MovieDetails> getWatchedListFromDB() {
-        String userid = Session.getInstance().getUserId();
-        ObservableList<MovieDetails> watchedMovies = FXCollections.observableArrayList();
-
-        String query = """
-                SELECT m.movieName, m.year, m.genre, m.type, m.seasons, m.imdb_Rating, um.userComment
-                FROM user_movies um
-                JOIN movies m ON um.movieid = m.movieid
-                WHERE um.userid = ? AND um.watched_Status = 'watched'
-                """;
-
-        try (Connection connectDB = new DatabaseConnection().getConnection();
-             PreparedStatement preparedStatement = connectDB.prepareStatement(query)) {
-
-            preparedStatement.setInt(1, Integer.parseInt(userid));
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                watchedMovies.add(new MovieDetails(
-                        resultSet.getString("movieName"),
-                        resultSet.getString("year"),
-                        resultSet.getString("genre"),
-                        resultSet.getString("imdb_Rating"),
-                        null, // rtRating (not used here)
-                        resultSet.getString("userComment"), // Using plot to hold userComment
-                        null, // posterUrl (not used here)
-                        resultSet.getString("type"),
-                        resultSet.getString("seasons")
-                ));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return watchedMovies;
-    }
-
-    private void populateWatchedMoviesTable() {
-        ObservableList<MovieDetails> watchedMovies = getWatchedListFromDB();
-        watchedListTableView.setItems(watchedMovies);
-    }
 
     public void navigateTo(String fxmlPath, String title, ActionEvent event) {
         try {
